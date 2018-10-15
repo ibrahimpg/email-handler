622223a0f0306bc95e32ac593e18d564f7e6a60d
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2018-10-15 00:18:37 -0800

    small fix

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post('/', (req, res) => {
+app.post('/message', (req, res) => {
   const mailOptions = {
     from: '"Ibrahim" test@ibrahimpg.com',
     to: req.body.email,
