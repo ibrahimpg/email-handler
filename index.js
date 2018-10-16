d69e7d2866c450408bdd4d0ff215eb2b5e9d04b8
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2018-10-15 19:21:45 -0800

    app.use to app.all on CORS

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use((req, res, next) => {
+app.all((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'POST');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
