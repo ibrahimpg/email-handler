5276267b548994f71f3a0d2d2cf4e41a151b4d7d
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2018-10-15 17:38:13 -0800

    html adjustments

--- index.js
+++ index.js
@@ -30,10 +30,13 @@ app.post('/message', (req, res) => {
     to: `${req.body.email}, ibrahim@ibrahimpg.com`,
     subject: 'Hey there!',
     html: `
-    <div style="width:100%; height:100%; background-color: #666; color: rgba(255, 255, 255, 0.9); padding: 10px;">
-      <p>${req.body.name},<br>I have received your message and will get back to you ASAP. Thank you for your interest!</p>
+    <div style="width:100%; height:100%; background-color: #666; padding: 10px;">
+      <p style="color: rgba(255, 255, 255, 0.9);">${req.body.name},</p>
+      <p style="color: rgba(255, 255, 255, 0.9);">I have received your message and will get back to you ASAP.
+      Thank you for your interest!</p>
       <hr>
-      <p style="padding:25px;">${req.body.message}</p>
+      <p style="color: rgba(255, 255, 255, 0.9); padding:25px;">${req.body.message}
+      <br>${req.body.email}</p>
     </div>`,
   };
   transporter.sendMail(mailOptions, (error) => {
