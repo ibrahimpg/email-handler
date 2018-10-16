625247a12c3f2108a9b4258bf3ff73c06b511724
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2018-10-15 19:33:09 -0800

    more fixes

--- index.js
+++ index.js
@@ -6,13 +6,9 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.all((req, res, next) => {
+app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-    res.status(200).json({});
-  }
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
