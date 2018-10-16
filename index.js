2fbfc5b9d83a66b9f194a8239692abaf454a40b4
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2018-10-15 19:40:37 -0800

    trying all again

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use((req, res, next) => {
+app.all((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
