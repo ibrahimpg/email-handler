e1fb254e39a91f6e209196d81882bb970d84f227
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2018-10-15 17:12:29 -0800

    various adjustments

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ app.use(express.json());
 app.post('/message', (req, res) => {
   const transporter = nodemailer.createTransport({
     host: 'mail.ibrahimpg.com',
-    port: 587,
+    port: 26,
     secure: false,
     auth: {
-      user: 'test@ibrahimpg.com',
+      user: 'ibrahim@ibrahimpg.com',
       pass: process.env.EMAIL_PW,
     },
     tls: {
@@ -20,12 +20,12 @@ app.post('/message', (req, res) => {
     },
   });
   const mailOptions = {
-    from: '"Ibrahim P.G." <test@ibrahimpg.com>',
-    to: `${req.body.email}, ipaboughalioum@alaska.edu`,
-    subject: 'Node Contact Request',
+    from: '"Ibrahim P.G." <ibrahim@ibrahimpg.com>',
+    to: `${req.body.email}, ibrahim@ibrahimpg.com`,
+    subject: 'Hey there!',
     html: `
     <div style="width:100%; height:100%; background-color: #666; color: rgba(255, 255, 255, 0.9); padding: 10px;">
-      <h1>${req.body.name}</h1>
+      <p>${req.body.name},</p>
       <p>I have received your message and will get back to you ASAP. Thank you for your interest!</p>
       <hr>
       <p style="padding-left:25px;">${req.body.message}</p>
