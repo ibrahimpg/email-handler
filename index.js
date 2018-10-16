d15c07252bf8d4f97039187099705331b1d6a7e0
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2018-10-15 19:54:37 -0800

    ..

--- index.js
+++ index.js
@@ -3,14 +3,18 @@ const nodemailer = require('nodemailer');
 
 const app = express();
 
-app.all((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    res.status(200).json({});
+  }
+  next();
+});
 
 app.post('/', (req, res) => {
   const transporter = nodemailer.createTransport({
