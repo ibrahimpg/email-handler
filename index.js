b2ef19810a3b76404c8bd018758f183f25eb51d5
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2018-10-15 19:14:44 -0800

    removing .end

--- index.js
+++ index.js
@@ -34,8 +34,8 @@ app.post('/message', (req, res) => {
       ${req.body.email}`,
   };
   transporter.sendMail(mailOptions)
-    .then(res.status(200).end())
-    .catch(res.status(500).end());
+    .then(res.status(200))
+    .catch(res.status(500));
 });
 
 const port = process.env.PORT || 8080;
