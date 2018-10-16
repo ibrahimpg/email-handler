c155f7a03db77ef3747aaf3f96b01fadacb79e39
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2018-10-15 17:17:36 -0800

    CORS

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.post('/message', (req, res) => {
   const transporter = nodemailer.createTransport({
     host: 'mail.ibrahimpg.com',
