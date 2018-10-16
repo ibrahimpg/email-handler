ce1e8c0f94edbdf17fff8dec978b460c7117c62e
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2018-10-15 19:09:36 -0800

    re-adding CORS

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.post('/message', (req, res) => {
   const transporter = nodemailer.createTransport({
     host: 'mail.ibrahimpg.com',
@@ -23,8 +29,7 @@ app.post('/message', (req, res) => {
     from: '"Ibrahim P.G." <ibrahim@ibrahimpg.com>',
     to: `${req.body.email}, ibrahim@ibrahimpg.com`,
     subject: 'Automatic reply from Ibrahim P.G.',
-    text: `${req.body.name}, I have received your message and will get back to you as soon as possible.
-    Thank you for your interest!
+    text: `${req.body.name}, I have received your message and will get back to you as soon as possible. Thank you for your interest!
       ${req.body.message}
       ${req.body.email}`,
   };
