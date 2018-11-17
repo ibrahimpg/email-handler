bf6d1a951f1fc43638f5810d0e0fae8a2a4bf4e2
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2018-11-16 17:21:06 -0900

    switch to CORS module instead of manually setting cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.post('/', (req, res) => {
   const transporter = nodemailer.createTransport({
