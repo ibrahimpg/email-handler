4af21e909aacd2805d289904c15755a4cda27048
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2018-11-16 17:09:24 -0900

    testing without CORS

--- index.js
+++ index.js
@@ -5,12 +5,6 @@ const app = express();
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
-
 app.post('/', (req, res) => {
   const transporter = nodemailer.createTransport({
     service: 'Outlook365',
